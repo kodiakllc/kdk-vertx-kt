336f7893645ff7fd0236dd4246648eb07e0b1f8e
Chris Hewison <hewisonchris@gmail.com>
2018-12-09 14:33:04 +0100

    A few examples added

---
+++ src/main/kotlin/lifecycle.kt
@@ -0,0 +1,54 @@
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
+
+fun main() {
+    withTimingLogged {
+        println("${Thread.currentThread()}:START")
+        val env = Env()
+        println("${Thread.currentThread()}:Start Env")
+        env.start()
+        println("${Thread.currentThread()}:Sleep for 5 secs")
+        Thread.sleep(2000)
+        println("${Thread.currentThread()}:Stop Env")
+        env.stop()
+        println("${Thread.currentThread()}:END")
+    }
+}
+
+class Env : CoroutineScope {
+    val job = Job()
+    override val coroutineContext: CoroutineContext
+        get() = job + Dispatchers.Default
+
+    fun start() {
+        repeat(3) {
+            launch { doWork(it) }
+        }
+    }
+
+    fun stop() {
+        job.cancel()
+    }
+}
+
+fun withTimingLogged(block: () -> Unit) {
+    val start = System.currentTimeMillis()
+    block.invoke()
+    println("${Thread.currentThread()}:DONE after ${(System.currentTimeMillis() - start) / 1000} secs")
+}
+
+suspend fun withTimingLoggedForDoWork(index: Int, block: suspend () -> Unit) {
+    val start = System.currentTimeMillis()
+    block.invoke()
+    println("$index==${Thread.currentThread()}:DONE after ${(System.currentTimeMillis() - start) / 1000} secs")
+}
+
+fun CoroutineScope.doWork(index: Int) {
+    launch {
+        println("${Thread.currentThread()}:doWork CALLED")
+        withTimingLoggedForDoWork(index) {
+            delay(1000)
+        }
+        println("${Thread.currentThread()}:doWork DONE")
+    }
+}
---
+++ src/main/kotlin/sync.kt
@@ -0,0 +1,18 @@
+import java.lang.Math.abs
+import kotlin.random.Random
+
+fun main() {
+    val start = System.currentTimeMillis()
+    println("${Thread.currentThread()}:Start")
+    listOf("A","B","C").map {
+        println(getStock(it))
+    }
+    println("${Thread.currentThread()}" +
+            ":Ended after ${(System.currentTimeMillis() - start) / 1000} secs")
+}
+
+private fun getStock(stock: String): String {
+    println("${Thread.currentThread()}:getStock for $stock")
+    Thread.sleep(1000)
+    return "Stock for $stock = ${abs(Random.nextInt())}"
+}
