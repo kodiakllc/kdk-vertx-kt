243686d9154ed9edca2267a30cf7ab516de485da
Nick Jaunich <panayao@gmail.com>
2024-09-07 14:54:12 -0400

    feature: Bring into modern era.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,31 +1,50 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.3.11"
+    kotlin("jvm") version "1.9.23"
+    id("io.gitlab.arturbosch.detekt") version "1.23.3"
 }
 
-group = "demo"
-version = "1.0-SNAPSHOT"
+group = "kdk"
+version = "1.0.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
 
 repositories {
     mavenCentral()
 }
 
+kotlin {
+    jvmToolchain(11)
+}
+
 dependencies {
-    compile(kotlin("stdlib-jdk8"))
+    implementation(kotlin("stdlib-jdk8"))
     implementation("io.github.microutils:kotlin-logging:1.6.22")
     implementation("ch.qos.logback:logback-classic:1.0.13")
+    // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.0.1")
-    implementation("io.vertx:vertx-web:3.6.0")
-    implementation("io.vertx:vertx-lang-kotlin:3.6.0")
-    implementation("io.vertx:vertx-lang-kotlin-coroutines:3.6.0")
-    testImplementation("io.vertx:vertx-junit5:3.6.0")
-    testRuntime("org.junit.jupiter:junit-jupiter-engine:5.2.0")
-    testCompile("io.mockk:mockk:1.8.13.kotlin13")
-    testCompile("org.junit.jupiter:junit-jupiter-api:5.3.2")
-    testCompile("org.testng:testng:6.14.3")
+    // Vert.x
+    implementation("io.vertx:vertx-web:3.9.16")
+    implementation("io.vertx:vertx-lang-kotlin:3.9.16")
+    implementation("io.vertx:vertx-lang-kotlin-coroutines:3.9.16")
+    testImplementation("io.vertx:vertx-junit5:3.9.16")
+    // MongoDB
+    implementation("org.mongodb:mongodb-driver-kotlin-coroutine:5.1.4")
+    // JUnit
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.2.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.2")
+    // MockK
+    testImplementation("io.mockk:mockk:1.8.13.kotlin13")
+    // TestNG
+    testImplementation("org.testng:testng:7.7.0")
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+    kotlinOptions {
+        jvmTarget = "11"
+    }
 }
